34d7124d53a57e1f2f46f5f4ae89ccaf2b5d2a85
unknown <ptnhan1201@gmail.com>
2025-09-21 11:56:30 +0700

    test image link

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "react-image-crop/dist/ReactCrop.css"
 import { Main } from "./components/Main/Main"
+import immunityIcon from "./assets/images/Immunity.png"
 
 export const App = () => {
     return (
         <>
             <main>
                 <Main />
+                <img src={immunityIcon} alt="immunity" />
             </main>
         </>
     )
